70cefee99f372062dda720441a400d7f8e5e00ee
Frank Mathy <frankmathy@gmail.com>
2020-10-08 14:06:30 +0200

    Fixed schema check

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const helmet = require('helmet');
+const yup = require('yup');
+const { nanoid } = require('nanoid');
 
 const app = express();
-
 app.use(helmet());
 app.use(morgan('tiny'));
 app.use(cors());
@@ -25,11 +26,57 @@ app.get('/:id', (req, res) => {
   //TODO redirect to URL
 });
 
-app.post('/url', (req, res) => {
-  // create a short url
+const schema = yup.object().shape({
+  slug: yup
+    .string()
+    .trim()
+    .matches(/^[\x21-\x7E]+$/i),
+  url: yup
+    .string()
+    .trim()
+    .url()
+    .required()
+});
+
+app.post('/url', (req, res, next) => {
+  let { slug, url } = req.body;
+  schema
+    .isValid({
+      slug,
+      url
+    })
+    .then(valid => {
+      console.log(`Schema valid = ${valid}`);
+      if (valid) {
+        if (!slug) {
+          slug = nanoid(5);
+        }
+        slug = slug.toLowerCase();
+        res.json({
+          slug,
+          url
+        });
+      } else {
+        console.log('Error');
+        next({ message: 'Incorrect slug format' });
+      }
+    });
+});
+
+app.use((error, req, res, next) => {
+  if (error.status) {
+    res.status(error.status);
+  } else {
+    res.status(500);
+  }
+  res.json({
+    message: error.message,
+    stack:
+      process.env.NODE_ENV === 'production' ? 'no stack available' : error.stack
+  });
 });
 
 const port = process.env.PORT || 1337;
 app.listen(port, () => {
-  console.log(`Lisening at http://localhost:${port}`);
+  console.log(`Listening at http://localhost:${port}`);
 });
